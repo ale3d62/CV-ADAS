f885fd8d381d70a8be4c59a8828e602fa8b6f023
ale3d62 <ale3d62@gmail.com>
2024-07-21 19:33:50 +0200

    changes

--- lane_finder_multitasking/car_finder_onnx.py
+++ lane_finder_multitasking/car_finder_onnx.py
@@ -4,54 +4,70 @@ import torch
 import numpy as np
 from time import time
 
-#Returns the bboxes of the acceptedClasses found in the frame 
 model = YOLO("v4n.onnx")
 
-ret = True
-vid = cv2.VideoCapture("test9.mp4")
+video_path = "../test_videos/"
 
-if(not vid):
-    print("no vid")
-    exit()
-
-while(ret):
-    #Get frame
-    ret, frame = vid.read()
-
-    if(not ret):
-        exit()
-
-    stl = time()
-    frame = cv2.resize(frame,(1280, 720))
-
-    #results = model(frame)
+# Indexes of the videos to use as input
+inputVideos = [*range(8, 27)] 
 
 
-    results = model.predict(source=frame, stream=True, imgsz=(384,672), device="cpu",name='v4_daytime', save=True, conf=0.25, iou=0.45, verbose=True)
-    
-    if(results):
-        #PROCESS LANES
-        mask = results[2][0].to(torch.uint8).cpu().numpy()
-        color_mask = np.stack([mask * 0, mask * 255, mask * 0], axis=-1)
-        alpha = 0.5  # transparency factor
-        #overlay
-        frame[np.any(color_mask != [0, 0, 0], axis=-1)] = (1 - alpha) * frame[
-                    np.any(color_mask != [0, 0, 0], axis=-1)] + alpha * color_mask[
-                                                                     np.any(color_mask != [0, 0, 0], axis=-1)]
 
-        #PROCESS BBOXES
-        for bbox in results[0][0].boxes.data.tolist():
-            x1, y1, x2, y2, score, class_id = bbox
+while len(inputVideos) > 0:
 
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 1)
+    # Get input video
+    vid = cv2.VideoCapture(video_path + 'test' + str(inputVideos[0]) + '.mp4')
+    inputVideos.pop(0)
+    ret = True
 
-        cv2.imshow('Frame',frame)
-        cv2.waitKey(1)
-    else:
-        print("no results")
+    while ret:
+        # Get frame
+        ret, frame = vid.read()
+
+        if not ret:
+            break
+
+        stl = time()
+        frame = cv2.resize(frame, (1280, 720))
+
+        # results = model(frame)
+
+        results = model.predict(
+            source=frame, 
+            stream=True, 
+            imgsz=(384, 672), 
+            device=[0], 
+            name='v4_daytime', 
+            save=True, 
+            conf=0.25, 
+            iou=0.45, 
+            verbose=True
+        )
+        
+        if results:
+            # PROCESS LANES
+            mask = results[2][0].to(torch.uint8).cpu().numpy()
+            color_mask = np.stack([mask * 0, mask * 255, mask * 0], axis=-1)
+            alpha = 0.5  # transparency factor
+            # overlay
+            frame[np.any(color_mask != [0, 0, 0], axis=-1)] = (
+                (1 - alpha) * frame[np.any(color_mask != [0, 0, 0], axis=-1)] + 
+                alpha * color_mask[np.any(color_mask != [0, 0, 0], axis=-1)]
+            )
+
+            # PROCESS BBOXES
+            for bbox in results[0][0].boxes.data.tolist():
+                x1, y1, x2, y2, score, class_id = bbox
+
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 1)
+
+            cv2.imshow('Frame', frame)
+            cv2.waitKey(1)
+        else:
+            print("no results")
+            cv2.imshow('Frame', frame)
+            cv2.waitKey(1)
+        
         cv2.imshow('Frame', frame)
         cv2.waitKey(1)
-    
-    cv2.imshow('Frame',frame)
-    cv2.waitKey(1)
-    print((time()-stl)*1000)
+        print((time() - stl) * 1000)
